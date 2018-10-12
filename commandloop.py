3127eaf46ce77cf9a3b979764010f3becdc75435
Ingramml <Ingramml@gmail.com>
2018-10-11 21:49:11 -0700

    Update commandloop.py

--- commandloop.py
+++ commandloop.py
@@ -2,23 +2,23 @@ import subprocess
 import sys
 import os
 import csv
-"""
-Loops through created CSV to launch command line bash command uplading file to US geocders.
 
-"""
-
-listofbash=open('/Users/michael/Documents/pmc1417files/bashlist.csv')
+listofbash=open('/Users/michael/Documents/pmc1417files/results/bashlist1417.csv') #files to be ran through command loop
 csvreader = csv.reader(listofbash)
 
+
 for row in csvreader:
-	if os.path.exists('/Users/michael/Documents/pmc1417files/results/'+row[0][223:]):
-		print(row[0][-27:]+ ' has been created')
+	x=row[0].index('geocoded/') #Determines the files names for checks#
+	print(row[0][(x+9):]) #Confirms file names for  
+	
+	if os.path.exists('/Users/michael/Documents/pmc1417files/results/geocoded/'+row[0][x+9:]):
+		print(row[0][(x+9):]+ ' has been created')
 	else:
-		print('creating ' +row[0][223:])
-		bashCommand = row[0]
+		print('creating ' +row[0][(x+9):])
+		bashCommand =row[0]
 		process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 		output, error = process.communicate()
-		print(row[0][223:] + ' has been done')
+		print(row[0][(x+9):] + ' has been done')
 	
 
 print('Geocoding complete')
