a2d926f41e1c2183f3befbb4e503f4f2842fb161
Michael Ingram <Ingramml@gmail.com>
2024-07-29 21:07:56 -0400

    changed csvfilesplitter sizing

--- csvfilesplitter.py
+++ csvfilesplitter.py
@@ -3,11 +3,11 @@ import sys
 Not my code found on/at https://blog.webnersolutions.com/split-large-csv-into-multiple-smaller-csv-files-with-python-script.
 Splits csv into multiple files working
 """
-fil='Geocodeneeded.csv'
+fil='/Users/michaelingram/Downloads/MI_Petition_signers.csv'
 csvfilename = open(fil, 'r').readlines()
 file = 1
 
 for j in range(len(csvfilename)):
-		if j % 2999 == 0:
-			open(str(fil[0:-4])+ str(file) + '.csv', 'w+').writelines(csvfilename[j:j+2999])
+		if j % 100000 == 0:
+			open(str(fil[0:-4])+ str(file) + '.csv', 'w+').writelines(csvfilename[j:j+100000])
 			file += 1
